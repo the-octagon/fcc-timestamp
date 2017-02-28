ed824cb0160749387e19a13c80c2d8686da3c299
Andrew King <andrew.f.king@gmail.com>
2017-02-28 20:38:49 +0000

    complete timestamp api project

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:date', function (req, res) {
+  var date = req.params.date;
+  if(isNaN(Number(date))) {
+    date = new Date(date);
+  } else {
+    date = new Date(date * 1000);
+  }
+  var time = {};
+  time.unix = Number(date.getTime()/1000);
+  if (isNaN(time.unix)) {
+    time.natural = null;
+  } else {
+    time.natural = (date).toString();
+  }
+  
+  res.send(time);
+  
+  
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Timestamp server is running');
 })
